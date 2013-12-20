a4e5e4fd796dbffe72dedc97408097b9fb5a199f
drewblount <dblount@reed.edu>
2013-12-19 22:07:41 -0800

    Added PrintHand
    Added functions like FindCardInPile which will facilitate the user typing, say, move(Jc, Qd)

--- nertz-client/nertz-client.go
+++ nertz-client/nertz-client.go
@@ -23,23 +23,127 @@ func Credentials() (string, string) {
     return strings.TrimSpace(username), strings.TrimSpace(password) // ReadString() leaves a trailing newline character
 }
 
+var faceMap = map[int]string {
+    1  : "A"
+    10 : "T"
+    11 : "J"
+    12 : "Q"
+    13 : "K"
+}
+
+var invFaceMap = map[string]int {
+    "A" : 1
+    "T" : 10
+    "J" : 11
+    "Q" : 12
+    "K" : 13
+}
+
+var suitMap = map[int]string {
+    1 : "h"
+    2 : "s"
+    3 : "d"
+    4 : "c"
+}
+
+var invSuitMap = map[string]int {
+    "h" : 1
+    "s" : 2
+    "d" : 3
+    "c" : 4
+} 
+
+
+func CardString(card *Card) (string) {
+    face := card.Value
+    suit := card.Suit
+    str := "%v"
+    if face > 1 && face < 10 {
+        str = fmt.Sprintf("%v", face)
+    }
+    else {
+    	str = fmt.Sprintf(faceMap[face], "%v")
+    }
+    str = fmt.Sprintf(str, suitMap[suit])
+    return str
+}
+
+// The way I read what used to be below was that
+// it'd print the mirror of what we had in our mockup, so I unmirrored it
 func PrintCardStack(cs *list.List, toShow int) {
-    stack := "[%v"
+    len := cs.Len()
+    stack := "%v"
     for e := cs.Front() ; e != nil ; e = e.Next() {
-        if toShow > 0 {
-            card := fmt.Sprintf("%v%v]%%v", e.Value.Value, e.Value.Suit))     
-            stack = fmt.Sprintf(stack, card)
-            toShow--
-        } else {
-            stack = fmt.Sprintf(stack, "]%v")
-        }   
+        stack = fmt.Sprintf(stack,"[%v")
+	if len - toShow <= 0 {
+	    // on the following line, I'm not sure how to get the card component of e
+            stack = fmt.Sprintf(stack, " ", CardString(e.Value), "%v")
+        }
+        len--
     }
-    stack = fmt.Sprintf(stack, "")
-    if stack == "[" {
-       fmt.Println("empty stack")
+    stack = fmt.Sprintf(stack, " ]")
+    if stack == "[ ]" {
+       fmt.Println("[ -empty- ]")
     } else {
-        fmt.Println(stack)
+       fmt.Println(stack)
     }
+}
+
+func PrintHand(hand *Hand) {
+    fmt.Println("River:")
+    for pile := range hand.River {
+    	PrintCardStack(pile, pile.Len())
+    }
+    // to print the stream and streampile, first print |stream|*"["
+    
+    fmt.Println("Stream:")
+    for i := 0; i < hand.Stream.Len ; i++ {
+    	fmt.Print("[")
+    }
+    PrintCardStack(hand.Streampile, hand.Streampile.Len())
+
+    fmt.Println("Nertz Pile:")
+    PrintCardStack(hand.NertzPile, 1)
+}
+
+FindCardInPile(face, suit int, pile *list.List) int {
+    count := 0
+    for e := pile.Front(); e != nil; e = e.next {
+        if e.Value.Value == face && e.Value.Suit == suit {
+	    return count
+	}
+        count ++
+    }
+    return -1
+}
+
+// returns (pilename, cardnum), with pilename taken from only
+// the piles accessible for moving cards. For piles such as the nertz
+// pile and the stream, only one card is accessible so only one card
+// is checked, and cardnum is 0 if that is the card being sought
+func FindCardInHand(face, suit int, hand *Hand) (string, int) {
+    pile := "error"
+    position := -1
+     
+    for i, RivPile := range(hand.River) {
+   	position = FindCardInPile(face, suit, RivPile)
+	if position != -1 {
+	    pile = fmt.Sprintf("River[%d]",i)
+	    return(pile, position)
+	}
+    }
+    // note: as I've written this, if only one card if face-up, that's the last card
+    c := hand.Nertzpile.Back().Value
+    if c.Value == face && c.Suit == suit {
+        return("Nertzpile", 0)
+    }
+    c = hand.Streampile.Back().Value
+    if c.Value == face && c.Suit == suit {
+        return("Streampile", 0)
+    }
+     	 
+    // if the card is not to be found, returns (error, -1)
+    return(pile, position)
 }
 
 func main() {
